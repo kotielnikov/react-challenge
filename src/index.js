017cecf3a9ba544c42784442d98b2e1424851419
Maksym Kotielnikov <maksym.kotielnikov@capmo.de>
2021-02-04 15:30:18 +0100

    Revert "Add mocked api"
    This reverts commit 2ee7367be6094fb8d4b45e346083141dceab2126.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-import { worker } from './mocks/browser';
-
-worker.start()
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
