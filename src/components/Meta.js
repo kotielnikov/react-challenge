0051a7c2835b1f6d8125fe24042b0277e9a6cb19
Maksym Kotielnikov <maksym.kotielnikov@capmo.de>
2021-02-04 15:31:11 +0100

    Fix meta

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -5,18 +5,15 @@ import React from "react";
  * This component should not be touched!
  */
 export const Meta = ({ address, owners }) => {
-  let i = 0;
-  while (i < 10e3) i += 1;
-
   return (
     <Typography variant="body2">
       {address.street} | {address.code} | {address.city}
       <br />
       Geschäftsführung: {owners.join(", ")}
-      {Array.from(Array(5000).keys()).map((i) => (
+      {Array.from(Array(3000).keys()).map((i) => (
         <span
           key={i}
-          style={{ position: "absolute", width: 0, height: 0, left: "-1000px" }}
+          style={{ position: "absolute", width: 1, height: 1, left: "-1000px" }}
         />
       ))}
     </Typography>
